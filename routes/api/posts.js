acf9e1fe5a03c602a2e4c88dc0ec8605abd2daeb
Stanislau Tukhto <s.tukhto.vironit@vironit.ru>
2018-09-12 17:37:38 +0300

    Implemented posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 // @route   GET api/posts/test
 // @desc    Test post route
 // @access  Public
 router.get('/test', (req, res) => {
 	res.json({msg: "Posts works"});
+});
+
+// @route   post api/posts/posts
+// @desc    Create post
+// @access  Private
+router.post('/posts', passport.authenticate('jwt', {session: false}), (req, res) => {
+
 });
 
 
